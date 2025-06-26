330dfa2744c2d0140360a481d673038876d3d818
NNoorFatima <noor.2205.nf@gmail.com>
2025-06-27 01:04:58 +0500

    added logout popup logic

--- MyApp/src/navigation/AppNavigator.tsx
+++ MyApp/src/navigation/AppNavigator.tsx
@@ -3,13 +3,22 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { NavigationContainer } from '@react-navigation/native';
 import UserProfile from '../screens/UserProfile/UserProfile';
 import EditProfile from '../screens/UserProfile/EditProfile';
-const Stack = createNativeStackNavigator();
+import Logout from '../screens/UserProfile/Logout';
+// Define route names inline here
+export type RootStackParamList = {
+  'Edit Profile': undefined;
+  'Profile': undefined;
+  'Logout': undefined;
+};
+
+const Stack = createNativeStackNavigator<RootStackParamList>();
 
 const AppNavigator = () => (
   <NavigationContainer>
-    <Stack.Navigator initialRouteName="Edit Profile">
-      {/* <Stack.Screen name="Profile" component={UserProfile} /> */}
+    <Stack.Navigator initialRouteName="Profile">
+      <Stack.Screen name="Profile" component={UserProfile} />
       <Stack.Screen name="Edit Profile" component={EditProfile} />
+      <Stack.Screen name="Logout" component={Logout} />
     </Stack.Navigator>
   </NavigationContainer>
 );
