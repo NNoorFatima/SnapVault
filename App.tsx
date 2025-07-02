5a7d2f204f463401271a927a49f3180671cf2517
N <nehawaseem846@gmail.com>
2025-07-02 23:17:50 +0500

    Sample check commit

--- App.tsx
+++ App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import AppNavigator from './src/navigation/AppNavigator';
+import CustomTextField from './src/components/CustomTextField';
 
 export default function App() {
-  return <AppNavigator />;
+  return <CustomTextField />;
 }
