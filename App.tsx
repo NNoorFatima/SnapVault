7473bc9e81924cb99dddae977b8329f2203b6769
N <nehawaseem846@gmail.com>
2025-07-03 15:28:59 +0500

    App Button Commit
    Custom button to ne reused throughout the screens

--- App.tsx
+++ App.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import AppNavigator from './src/navigation/AppNavigator';
-import CustomTextField from './src/components/CustomTextField';
 
 export default function App() {
-  return <CustomTextField />;
+  return <AppNavigator/>;
 }
