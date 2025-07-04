93de4ef1c6ec950ed11a84629281c51bc197484e
N <nehawaseem846@gmail.com>
2025-07-04 13:20:40 +0500

    fixed Splash Screen Commit

--- App.tsx
+++ App.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import AppNavigator from './src/navigation/AppNavigator';
 import Toast from 'react-native-toast-message';
+import SplashScreen from './src/screens/Splash/SplashScreen';
 
 export default function App() {
   return (
     <>
-      <AppNavigator />
-      <Toast />
+      <SplashScreen/>
     </>
   );
 }
+
+//<AppNavigator />
+ //     <Toast />
