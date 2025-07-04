dc91bc9e583365517940bec2e6cf82d675ef046e
NNoorFatima <noor.2205.nf@gmail.com>
2025-07-04 16:34:40 +0500

    fixed contact us styling

--- src/screens/ContactUs/ContactUs.tsx
+++ src/screens/ContactUs/ContactUs.tsx
@@ -107,7 +107,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#000',
-    paddingBottom: 55,
+    // paddingBottom: 55,
   },
   scrollView: {
     flex: 1,
