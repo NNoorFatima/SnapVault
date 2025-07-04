d5c364dcf77e6bfb7af94760a40fa58da4f6f759
N <nehawaseem846@gmail.com>
2025-07-04 13:23:32 +0500

    Correct App.tsx Commit

--- App.tsx
+++ App.tsx
@@ -1,15 +1,11 @@
 import React from 'react';
 import AppNavigator from './src/navigation/AppNavigator';
 import Toast from 'react-native-toast-message';
-import SplashScreen from './src/screens/Splash/SplashScreen';
-
 export default function App() {
   return (
     <>
-      <SplashScreen/>
+      <AppNavigator />
+      <Toast />
     </>
   );
 }
-
-//<AppNavigator />
- //     <Toast />
