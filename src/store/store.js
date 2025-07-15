76507498a1f8d3a2196a26f64828cc4250695dd1
N <nehawaseem846@gmail.com>
2025-07-15 21:08:49 +0500

    Import fix Commit

--- src/store/store.js
+++ src/store/store.js
@@ -1,6 +1,6 @@
 import { configureStore } from '@reduxjs/toolkit';
 import authReducer from './slices/authSlice';
-import groupsReducer from './slices/groupsSlice';
+import groupsReducer from './slices/groupSlice';
 import photosReducer from './slices/photosSlice';
 
 const store = configureStore({
